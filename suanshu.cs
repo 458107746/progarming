ca831c84cb1728179b289dece06d2df2ad51aee9
usera001 <hjbaogui@163.com>
2022-09-23 15:13:32 +0800

    this is my love

---
+++ suanshu.cs
@@ -0,0 +1,28 @@
+public static class suanshu
+{
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+	public int max(int a,int b)
+	{
+		if(a>b)
+			return a:
+		else
+			return b;
+	}
+	public int min(int a,int b)
+	{
+		if(a<b)
+			return a;
+		else
+			return b;
+	}
+	public int minus(int a,int b)
+	{
+		if(a>b)
+			return a-b;
+		else
+			return b-a;
+	}
+}
